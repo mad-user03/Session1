3c386e3eb1511bc6842d8b5a47f1b25ddbb95f5a
admin <123>
2022-02-10 15:22:08 +0300

    session6

--- app/src/main/java/com/example/session1/Main.kt
+++ app/src/main/java/com/example/session1/Main.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isGone
+import androidx.core.view.isInvisible
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +17,21 @@ class Main : AppCompatActivity() {
     fun WatchButton(view: View) {
         val intent = Intent(this, MovieScene::class.java)
         startActivity(intent)
+    }
+
+    fun onPlay(view: View) {
+        image.setImageResource(R.drawable.playimage)
+        imageView12.setImageResource(R.drawable.line)
+        imageButton4.setImageResource(R.drawable.playbuttonsecond)
+        textView9.visibility
+        scrollView.height.compareTo(768)
+    }
+
+
+    fun imClick(view: View) {
+        image.setImageResource(R.drawable.lastmovie)
+        imageView12.setImageResource(R.drawable.gradientforlowline)
+        imageButton4.setImageResource(R.drawable.gradientforlowline)
+        imageView22.setImageResource(R.drawable.rectangle)
     }
 }
--- app/src/main/java/com/example/session1/RetrofitInterface.kt
+++ app/src/main/java/com/example/session1/RetrofitInterface.kt
@@ -3,9 +3,12 @@ package com.example.session1
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface RetrofitInterface {
-//@GET("/usermovies")
-//suspend fun usermovies(@Query("lastView")) : Call<List<ModelTwo>>
+//    @GET("/usermovies")
+//suspend fun usermovies() :  Call<List<ModelTwo>>{
+//
+//}
 }
